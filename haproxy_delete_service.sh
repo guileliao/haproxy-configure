2321ee24f86111e117bbbae987d2b7d6c69640ea
guile liao <guileliao@users.noreply.github.com>
2016-11-25 09:22:09 +0800

    Update haproxy_delete_service.sh

--- haproxy_delete_service.sh
+++ haproxy_delete_service.sh
@@ -5,5 +5,5 @@
 if [[ $1 = "" ]];then
 	echo "error:service name is null." && exit
 else
-	sed -i /#$1_begin#/,/#$1_end#/d haproxy.cfg && sed -i /$1/d haproxy.cfg
+	sed -i /#$1_begin#/,/#$1_end#/d /etc/haproxy/haproxy.cfg && sed -i /$1/d /etc/haproxy/haproxy.cfg
 fi
