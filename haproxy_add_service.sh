2207e161a4f9def9f1d2582e505273d0452290e3
guile liao <guileliao@users.noreply.github.com>
2016-11-25 09:28:29 +0800

    Update haproxy_add_service.sh

--- haproxy_add_service.sh
+++ haproxy_add_service.sh
@@ -10,13 +10,13 @@ if [[ $1 = "" ]];then
 	echo "error:service name is null." && exit
 fi
 if [[ $(grep '^acl' haproxy.cfg|grep ""$1"$") = "" ]];then
-	sed -i /^#ACL#/a\ "acl $1_acl path_beg \-i \/$1" haproxy.cfg
+	sed -i /^#ACL#/a\ "acl $1_acl path_beg \-i \/$1" /etc/haproxy/haproxy.cfg
 fi
 if [[ $(grep "^use_backend" haproxy.cfg|grep ""$1_acl"$") = "" ]];then
-	sed -i /^#USE_BACKEND#/a\ "use_backend $1_backend \if $1_acl" haproxy.cfg
+	sed -i /^#USE_BACKEND#/a\ "use_backend $1_backend \if $1_acl" /etc/haproxy/haproxy.cfg
 fi
-sed -i /#$1_begin#/,/#$1_end#/d haproxy.cfg
-cat>>haproxy.cfg<<EOF
+sed -i /#$1_begin#/,/#$1_end#/d /etc/haproxy/haproxy.cfg
+cat>>/etc/haproxy/haproxy.cfg<<EOF
 #$1_begin#	
 backend $1_backend
 balance roundrobin
