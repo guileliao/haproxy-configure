16e1c27f30c380524e707afac52c8b00859dc9da
guile liao <guileliao@users.noreply.github.com>
2016-11-25 09:37:40 +0800

    Update haproxy_add_service.sh

--- haproxy_add_service.sh
+++ haproxy_add_service.sh
@@ -9,10 +9,10 @@
 if [[ $1 = "" ]];then
 	echo "error:service name is null." && exit
 fi
-if [[ $(grep '^acl' haproxy.cfg|grep ""$1"$") = "" ]];then
+if [[ $(grep '^acl' /etc/haproxy/haproxy.cfg|grep ""$1"$") = "" ]];then
 	sed -i /^#ACL#/a\ "acl $1_acl path_beg \-i \/$1" /etc/haproxy/haproxy.cfg
 fi
-if [[ $(grep "^use_backend" haproxy.cfg|grep ""$1_acl"$") = "" ]];then
+if [[ $(grep "^use_backend" /etc/haproxy/haproxy.cfg|grep ""$1_acl"$") = "" ]];then
 	sed -i /^#USE_BACKEND#/a\ "use_backend $1_backend \if $1_acl" /etc/haproxy/haproxy.cfg
 fi
 sed -i /#$1_begin#/,/#$1_end#/d /etc/haproxy/haproxy.cfg
