0ec8998496cba0f4d0999cb07ba2b1a068af56b8
guile liao <guileliao@users.noreply.github.com>
2017-05-09 10:53:08 +0800

    Add files via upload

--- haproxy_add_service.sh
+++ haproxy_add_service.sh
@@ -1,21 +1,26 @@
 #!/usr/bin/env bash
 
 #$1=service name;
-#$2=node name;
-#$3=node ipaddress;
-#$4=node port;
-#$5=weight value;
+#$2=proxyurl value
 
-if [[ $1 = "" ]];then
-	echo "error:service name is null." && exit
+if [ -z "$1" ];then
+	echo "error:service name is null." && exit 2
 fi
-if [[ $(grep '^acl' /etc/haproxy/haproxy.cfg|grep ""$1"$") = "" ]];then
+
+if [ -z "$2" ] && [ -z "$(grep '^acl' /etc/haproxy/haproxy.cfg|grep ""$1"$")" ];then
 	sed -i /^#ACL#/a\ "acl $1_acl path_reg \-i \/$1/" /etc/haproxy/haproxy.cfg
 fi
-if [[ $(grep "^use_backend" /etc/haproxy/haproxy.cfg|grep ""$1_acl"$") = "" ]];then
+
+if [ -n "$2" ] && [ -z "$(grep '^acl' /etc/haproxy/haproxy.cfg|grep ""$1"$")" ];then
+	sed -i /^#ACL#/a\ "acl $1_acl path_reg \-i \/$2/$1/" /etc/haproxy/haproxy.cfg
+fi
+
+if [ -z "$(grep "^use_backend" /etc/haproxy/haproxy.cfg|grep ""$1_acl"$")" ];then
 	sed -i /^#USE_BACKEND#/a\ "use_backend $1_backend \if $1_acl" /etc/haproxy/haproxy.cfg
 fi
+
 sed -i /#$1_begin#/,/#$1_end#/d /etc/haproxy/haproxy.cfg
+
 cat>>/etc/haproxy/haproxy.cfg<<EOF
 #$1_begin#	
 backend $1_backend
